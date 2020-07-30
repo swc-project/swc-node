81140210f99e68d97ec3b7e29d8e2e0ff9863364
LongYinan <longyinan.brooklyn@bytedance.com>
2020-07-30 17:43:02 +0800

    ci: fix pre publish script

--- packages/core/publish.js
+++ packages/core/publish.js
@@ -15,8 +15,8 @@ updatePackageJson(path.join(__dirname, 'package.json'), {
 
 for (const name of platforms) {
   const pkgDir = path.join(__dirname, 'npm', `core-${name}`)
-  const filename = path.join(`bindings-${name}`, `swc.${name}.node`)
-  const bindingFile = fs.readFileSync(path.join(__dirname, filename))
+  const filename = `swc.${name}.node`
+  const bindingFile = fs.readFileSync(path.join(__dirname, `bindings-${name}`, filename))
   fs.writeFileSync(path.join(pkgDir, filename), bindingFile)
   execSync('npm publish', {
     cwd: pkgDir,
