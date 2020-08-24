c6e8a7858f504eaabf07254cf7e3ec42eee432eb
LongYinan <longyinan.brooklyn@bytedance.com>
2020-08-24 17:49:13 +0800

    fix(core): linux musl addon loader

--- packages/core/index.js
+++ packages/core/index.js
@@ -3,14 +3,37 @@ const { platform } = require('os')
 const { loadBinding } = require('@node-rs/helper')
 
 let bindings
+let linuxError = null
 
 try {
   bindings = loadBinding(__dirname, 'swc')
 } catch (e) {
+  const platformName = platform()
   try {
-    bindings = require(`@swc-node/core-${platform()}`)
+    bindings = require(`@swc-node/core-${platformName}`)
   } catch (e) {
-    throw new TypeError('Not compatible with your platform. Error message: ' + e.message)
+    if (platformName !== 'linux') {
+      throw new TypeError('Not compatible with your platform. Error message: ' + e.message)
+    } else {
+      linuxError = e
+    }
+  }
+}
+
+if (!bindings) {
+  try {
+    require.resolve('@swc-node/core-linux-musl')
+  } catch (e) {
+    throw new TypeError(
+      `Could not load @swc-node/core-linux, You may need add @swc-node/core-linux-musl to optionalDependencies of your project`,
+    )
+  }
+  try {
+    bindings = require('@swc-node/core-linux-musl')
+  } catch (e) {
+    throw new TypeError(
+      `Linux glibc version load error: ${linuxError.message}; Linux musl version load error: Error message: ${e.message}`,
+    )
   }
 }
 
@@ -68,5 +91,5 @@ module.exports = {
     return bindings.transform(source, path, Buffer.from(JSON.stringify(swcOptions)))
   },
 
-  SWC_VERSION: 'e619144',
+  SWC_VERSION: '1.2.21',
 }
