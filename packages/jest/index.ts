77cc3d8ea82728b9b486e1b5fd898f82180c3f37
LongYinan <longyinan.brooklyn@bytedance.com>
2021-01-25 10:58:23 +0800

    feat(jest): cache transpile result to avoid source code to be compiled mult times
    Close https://github.com/Brooooooklyn/swc-node/issues/178

--- packages/jest/index.ts
+++ packages/jest/index.ts
@@ -1,11 +1,26 @@
+import { createHash } from 'crypto'
+
 import { transformJest } from '@swc-node/core'
+import { Output } from '@swc/core'
+
+const Cache = new Map<string, Output>()
 
 export = {
   process(src: string, path: string, jestConfig: any) {
     const [, , transformOptions = {}] =
       (jestConfig.transform || []).find(([, transformerPath]: [string, string]) => transformerPath === __filename) || []
     if (/\.(t|j)sx?$/.test(path)) {
-      return transformJest(src, path, transformOptions)
+      // sha1 is fast, and we don't care about security here
+      const cacheHash = createHash('sha1')
+      cacheHash.update(src)
+      const hash = cacheHash.digest('hex')
+      const cacheKey = `${path}-${hash}`
+      if (Cache.has(cacheKey)) {
+        return Cache.get(cacheKey)
+      }
+      const output = transformJest(src, path, transformOptions)
+      Cache.set(cacheKey, output)
+      return output
     }
     return src
   },
