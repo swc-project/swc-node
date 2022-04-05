3062413e464a5b5706c5ac4912f43ef7451fe73b
David Register <dmregister1@gmail.com>
2022-04-05 01:56:08 -0400

    feat(register): always resolve full file path for tsconfig and add paths if available (#646)
    * feat(register): always resolve to full path for tsConfig
    
    * feat(register): add paths to swc config if available on the compiler options
    
    * test: update snap
    
    * chore: add generated snapshot
    
    * test: remove snapshot test as jest and ava have conflicts

--- packages/register/__test__/read-default-config.spec.ts
+++ packages/register/__test__/read-default-config.spec.ts
@@ -2,6 +2,7 @@ import { join, dirname } from 'path'
 
 import test from 'ava'
 import { omit } from 'lodash'
+import sinon from 'sinon'
 import * as ts from 'typescript'
 
 import { readDefaultTsConfig } from '../read-default-tsconfig'
@@ -44,5 +45,41 @@ test('should RESPECT tsconfig path in subdirectory', (t) => {
   const defaultOptions = readDefaultTsConfig()
   const { config } = ts.readConfigFile(configPath, ts.sys.readFile)
   const { options } = ts.parseJsonConfigFileContent(config, ts.sys, dirname(configPath))
+  t.deepEqual(omit(defaultOptions, 'files'), options)
+})
+
+test('should return default compiler options when the tsConfigPath is invalid', (t) => {
+  const configPath = join(__dirname, 'invalid', 'tsconfig.json')
+
+  delete process.env.SWC_NODE_PROJECT
+  delete process.env.TS_NODE_PROJECT
+  process.env.TS_NODE_PROJECT = configPath
+
+  const defaultOptions = readDefaultTsConfig()
+  t.deepEqual(defaultOptions, {
+    target: ts.ScriptTarget.ES2018,
+    module: ts.ModuleKind.CommonJS,
+    moduleResolution: ts.ModuleResolutionKind.NodeJs,
+    sourceMap: true,
+    esModuleInterop: true,
+  })
+})
+
+test('should RESPECT tsconfig path in subdirectory with a relative path', (t) => {
+  const configPath = join('..', '__test__', 'tsconfig.spec.json')
+  const fullConfigPath = join(__dirname, 'tsconfig.spec.json')
+
+  delete process.env.SWC_NODE_PROJECT
+  delete process.env.TS_NODE_PROJECT
+  process.env.TS_NODE_PROJECT = configPath
+
+  sinon.replace(process, 'cwd', () => __dirname)
+
+  const defaultOptions = readDefaultTsConfig()
+
+  sinon.restore()
+
+  const { config } = ts.readConfigFile(fullConfigPath, ts.sys.readFile)
+  const { options } = ts.parseJsonConfigFileContent(config, ts.sys, dirname(fullConfigPath))
   t.deepEqual(omit(defaultOptions, 'files'), options)
 })
