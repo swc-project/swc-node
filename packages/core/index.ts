767fcd9642de4e26265d241a5141f000a7d6f11a
Daniel Syddall <daniel.syddall@in-part.co.uk>
2023-11-12 23:32:40 +0000

    fix: respect sourceMaps swcrc value

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -71,7 +71,11 @@ function transformOption(path: string, options?: Options, jest = false): SwcOpti
               }
             : undefined),
         },
-    sourceMaps: jest || typeof opts.sourcemap === 'undefined' ? 'inline' : opts.sourcemap,
+    sourceMaps: options?.swc?.swcrc
+      ? undefined
+      : jest || typeof opts.sourcemap === 'undefined'
+      ? 'inline'
+      : opts.sourcemap,
     inlineSourcesContent: true,
     swcrc: false,
     ...(options?.swc ?? {}),
